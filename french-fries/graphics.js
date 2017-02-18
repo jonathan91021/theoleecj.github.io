29f4804a4cb89b14bbe7a6a83623ea042dba3d9f
Theo Lee <theodorelcj@gmail.com>
2017-02-18 15:44:13 +0800

    Create graphics.js

---
+++ french-fries/graphics.js
@@ -0,0 +1,52 @@
+var bots = [
+  {
+    botCoords: 0,
+    botHealth: 10
+  }
+];
+var fryHealth = 50, points = 0, fryCoords = 0;
+var lasers = {};
+var canvasContext = null, canvas = null;
+
+function StartGame() {
+  document.getElementById("Overlay").style.display = "none";
+  InitBots();
+}
+
+function StartPaint() {
+  canvas = document.getElementById("Game");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvasContext = canvas.getContext("2d");
+  InitFry();
+}
+
+function Repaint() {
+}
+
+function InitBots() {
+  var botsDrawn = 0, botsToDraw = bots.length;
+  var botImage = new Image();
+  botImage.onload = function() {
+   while (botsToDraw > botsDrawn) {
+      var currentBot = bots[botsDrawn];
+      canvasContext.drawImage(botImage, currentBot.botCoords + 10, -15, 150, 170);
+      botsDrawn++;
+    }
+  };
+  
+  botImage.src = "assets/Normal.png";
+}
+
+function InitFry() {
+  var fryImage = new Image();
+  fryImage.onload = function() {
+    canvasContext.drawImage(fryImage, fryCoords + 10, canvas.height - 200, 200, 200);
+  };
+  
+  fryImage.src = "assets/Normal Fry.png";
+}
+
+function ShootBot() {
+
+}
