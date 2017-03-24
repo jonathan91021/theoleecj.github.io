98b57a506b76bb159bfff9e09caaa01df5724221
Theo Lee <theodorelcj@gmail.com>
2017-03-24 15:06:48 +0800

    Update game.js

--- mastermind/game.js
+++ mastermind/game.js
@@ -1,5 +1,5 @@
 //Store computer's randomly generated series
-var compSeries = ["white", "white", "white", "white"];
+var compSeries = ["lightgrey", "lightgrey", "lightgrey", "lightgrey"];
 
 //Store results that computer returned for user's guess
 var compsResults = {
@@ -18,4 +18,29 @@ while (compsResultsPopulated < 12) {
 function StartAsGuesser() {
   document.getElementById("MainMenu").style.display = "none";
   document.getElementById("Board").style.display = "block";
+  
+  LoadComputerAsSetter();
+  LoadDefaultPieces();
+}
+
+//Set computer's guesses
+function LoadComputerAsSetter() {
+  
+}
+
+//Place default blank pieces onto board
+function LoadDefaultPieces() {
+  var rows = 12, rowsRendered = 0;
+  
+  while (rowsRendered < rows) {
+    rowsRendered++;
+    
+    //Generate 4 blank spaces for placement
+    var blankSpaces = document.createElement("circle");
+    blankSpaces.setAttribute("cx", (100 + (rowsRendered * 45)));
+    blankSpaces.setAttribute("cy", "730");
+    blankSpaces.setAttribute("r", "20");
+    
+    //Generate spaces for computer to tell user result of their guess
+  }
 }
